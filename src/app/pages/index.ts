5480c512e5e498037be80a0f5e5462cb1187a9b2
batbrain9392 <debmallyabhattacharya@gmail.com>
2019-09-03 20:13:19 +0530

    feat(speakers): speaker cmp added

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -1,16 +1,19 @@
 import { HomeComponent } from './home/home.component';
 import { ScheduleComponent } from './schedule/schedule.component';
 import { SpeakersComponent } from './speakers/speakers.component';
+import { SpeakerComponent } from './speakers/speaker/speaker.component';
 import { TeamComponent } from './team/team.component';
 
 export { HomeComponent } from './home/home.component';
 export { ScheduleComponent } from './schedule/schedule.component';
 export { SpeakersComponent } from './speakers/speakers.component';
+export { SpeakerComponent } from './speakers/speaker/speaker.component';
 export { TeamComponent } from './team/team.component';
 
 export const pages = [
   HomeComponent,
   ScheduleComponent,
   SpeakersComponent,
+  SpeakerComponent,
   TeamComponent
 ];
--- src/app/pages/speakers/speakers.component.ts
+++ src/app/pages/speakers/speakers.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { ISpeaker } from 'src/app/models';
 
 @Component({
   selector: 'app-speakers',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./speakers.component.scss']
 })
 export class SpeakersComponent implements OnInit {
+  speakers$: Observable<ISpeaker>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.speakers$ = of([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
   }
 
+  ngOnInit() {}
 }
