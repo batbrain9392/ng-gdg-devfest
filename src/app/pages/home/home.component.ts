b7e821136e26332ba731b18af52905cfe46bd37b
batbrain9392 <debmallyabhattacharya@gmail.com>
2019-09-05 17:40:00 +0530

    fix(home): partner sharing removed, uppercase button text

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { WebShareService } from '../../shared/services';
 
 @Component({
   selector: 'app-home',
@@ -8,13 +7,4 @@ import { WebShareService } from '../../shared/services';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
-  constructor(private webShareService: WebShareService) {}
-
-  onShareClick(partner: any) {
-    this.webShareService.share({
-      title: 'Partner',
-      text: 'Partner | Type',
-      url: 'https://gdg-devfest-ng.herokuapp.com/home'
-    });
-  }
 }
