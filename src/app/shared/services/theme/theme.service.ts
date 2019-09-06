32a4fc8224b43061ca1aad3501134cfcb8b076f4
batbrain9392 <debmallyabhattacharya@gmail.com>
2019-09-06 14:17:52 +0530

    fix(app): minor changes

--- src/app/shared/services/theme/theme.service.ts
+++ src/app/shared/services/theme/theme.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeService {
   private isDarkTheme = new BehaviorSubject(false);
-  isDarkTheme$ = this.isDarkTheme.asObservable();
+  isDarkTheme$ = this.isDarkTheme.asObservable().pipe(shareReplay());
 
   toggleDarkTheme() {
     this.isDarkTheme.next(!this.isDarkTheme.getValue());
