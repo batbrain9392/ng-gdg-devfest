b634a6d029f72ec1fa3c06e969e34867c122d6f8
Debmallya Bhattacharya <debmallyabhattacharya@gmail.com>
2019-09-07 21:42:12 +0530

    feat(anim): added ngx-animate for future use.

---
+++ src/app/shared/animations/index.ts
@@ -0,0 +1,6 @@
+import { trigger, transition, useAnimation } from '@angular/animations';
+import { bounce } from 'ngx-animate';
+
+export const animBounce = trigger('bounce', [
+  transition('* => *', useAnimation(bounce))
+]);
