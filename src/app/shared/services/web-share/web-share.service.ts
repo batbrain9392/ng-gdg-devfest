2d9ee094c28aaf84d61198403945707e9d3d827c
Debmallya Bhattacharya <debmallyabhattacharya@gmail.com>
2019-09-10 08:57:29 +0530

    fix(share): needs better SoC, DRY

--- src/app/shared/services/web-share/web-share.service.ts
+++ src/app/shared/services/web-share/web-share.service.ts
@@ -5,28 +5,20 @@ import { IWebShare } from './i-web-share';
   providedIn: 'root'
 })
 export class WebShareService {
-  readonly shareFunction = navigator && (navigator as any).share;
-  readonly isWebShareAvailable = !!this.shareFunction;
+  readonly isWebShareAvailable = !!(navigator && (navigator as any).share);
 
   constructor() {
     this.apiAvailabilityLog();
   }
 
   share({ title, text, url }: IWebShare) {
-    alert(JSON.stringify(this.shareFunction));
-    alert(this.isWebShareAvailable);
-    alert(JSON.stringify(navigator && (navigator as any).share));
-    if (this.isWebShareAvailable) {
-      (navigator as any)
-        .share({ title, text, url })
-        .then(() => console.log('Successful share'))
-        .catch((error: any) => console.log('Error sharing', error));
-    } else {
-      this.apiAvailabilityLog();
-    }
+    (navigator as any)
+      .share({ title, text, url })
+      .then(() => console.log('Shared'))
+      .catch((error: any) => console.log('Error sharing', error));
   }
 
   private apiAvailabilityLog() {
-    console.log('Web-share Api available -', this.isWebShareAvailable);
+    console.log('Web-share api available -', this.isWebShareAvailable);
   }
 }
