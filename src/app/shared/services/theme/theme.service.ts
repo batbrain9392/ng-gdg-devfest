e053376d2458cf1dd19b2a67ee977a8be258e7ee
Debmallya Bhattacharya <debmallyabhattacharya@gmail.com>
2019-09-10 07:36:21 +0530

    feat(theme): change theme-color on theme change

--- src/app/shared/services/theme/theme.service.ts
+++ src/app/shared/services/theme/theme.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 import { BehaviorSubject } from 'rxjs';
 import { shareReplay } from 'rxjs/operators';
 
@@ -9,7 +10,12 @@ export class ThemeService {
   private readonly isDarkTheme = new BehaviorSubject(false);
   readonly isDarkTheme$ = this.isDarkTheme.asObservable().pipe(shareReplay());
 
+  constructor(private meta: Meta) {}
+
   toggleDarkTheme() {
-    this.isDarkTheme.next(!this.isDarkTheme.getValue());
+    const isDarkTheme = !this.isDarkTheme.getValue();
+    this.isDarkTheme.next(isDarkTheme);
+    const themeColor = isDarkTheme ? '#212121' : '#1976d2';
+    this.meta.updateTag({ name: 'theme-color', content: themeColor });
   }
 }
