712efd8d6fdc9a7f1a791e834c606338b0401a46
batbrain9392 <debmallyabhattacharya@gmail.com>
2019-09-10 15:39:35 +0530

    feat(app): dark mode based on device's preference

--- src/app/shared/services/theme/theme.service.ts
+++ src/app/shared/services/theme/theme.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Meta } from '@angular/platform-browser';
+import { MediaMatcher } from '@angular/cdk/layout';
 import { BehaviorSubject, combineLatest } from 'rxjs';
 import { shareReplay } from 'rxjs/operators';
 import { OfflineService } from '../offline/offline.service';
@@ -8,13 +9,36 @@ import { OfflineService } from '../offline/offline.service';
   providedIn: 'root'
 })
 export class ThemeService {
-  private readonly isDarkTheme = new BehaviorSubject(false);
+  private readonly preferDarkTheme = this.mediaMatcher.matchMedia(
+    '(prefers-color-scheme: dark)'
+  );
+  private readonly isDarkTheme = new BehaviorSubject(
+    this.preferDarkTheme.matches
+  );
   readonly isDarkTheme$ = this.isDarkTheme.asObservable().pipe(shareReplay());
 
-  constructor(offlineService: OfflineService, meta: Meta) {
-    combineLatest(this.isDarkTheme$, offlineService.isOffline$).subscribe(
+  constructor(
+    private offlineService: OfflineService,
+    private meta: Meta,
+    private mediaMatcher: MediaMatcher
+  ) {
+    this.preferDarkTheme.addEventListener('change', ({ matches }) =>
+      this.isDarkTheme.next(matches)
+    );
+    this.watchAndUpdateMetaThemeColor();
+    this.watchDeviceColorSchemeAndUpdateTheme();
+  }
+
+  watchDeviceColorSchemeAndUpdateTheme() {
+    this.preferDarkTheme.addEventListener('change', ({ matches }) =>
+      this.isDarkTheme.next(matches)
+    );
+  }
+
+  watchAndUpdateMetaThemeColor() {
+    combineLatest(this.isDarkTheme$, this.offlineService.isOffline$).subscribe(
       ([isDarkTheme, isOffline]) =>
-        meta.updateTag({
+        this.meta.updateTag({
           name: 'theme-color',
           content: isOffline ? '#bdbdbd' : isDarkTheme ? '#212121' : '#1976d2'
         })
