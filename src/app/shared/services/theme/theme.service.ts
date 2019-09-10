454493123a4e47a33d11b2a726b3df919b015f12
Debmallya Bhattacharya <debmallyabhattacharya@gmail.com>
2019-09-10 07:52:10 +0530

    fix(theme): sync in meta and app theme change timing

--- src/app/shared/services/theme/theme.service.ts
+++ src/app/shared/services/theme/theme.service.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Meta } from '@angular/platform-browser';
 import { BehaviorSubject } from 'rxjs';
-import { shareReplay } from 'rxjs/operators';
+import { tap, shareReplay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeService {
   private readonly isDarkTheme = new BehaviorSubject(false);
-  readonly isDarkTheme$ = this.isDarkTheme.asObservable().pipe(shareReplay());
+  readonly isDarkTheme$ = this.isDarkTheme.asObservable().pipe(
+    tap(isDarkTheme =>
+      this.meta.updateTag({
+        name: 'theme-color',
+        content: isDarkTheme ? '#212121' : '#1976d2'
+      })
+    ),
+    shareReplay()
+  );
 
   constructor(private meta: Meta) {}
 
   toggleDarkTheme() {
-    const isDarkTheme = !this.isDarkTheme.getValue();
-    this.isDarkTheme.next(isDarkTheme);
-    const themeColor = isDarkTheme ? '#212121' : '#1976d2';
-    this.meta.updateTag({ name: 'theme-color', content: themeColor });
+    this.isDarkTheme.next(!this.isDarkTheme.getValue());
   }
 }
