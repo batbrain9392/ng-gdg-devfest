c2164535ef2513c59639e573aaa258fec07c5242
Debmallya Bhattacharya <debmallyabhattacharya@gmail.com>
2019-09-12 02:46:14 +0530

    fix(app): subsink removed cuz error in ssr

--- src/app/shared/services/seo/seo.service.ts
+++ src/app/shared/services/seo/seo.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Title, Meta } from '@angular/platform-browser';
+import { Router, RouterEvent, NavigationEnd } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 import { PlatformService } from '../platform/platform.service';
+import { IRoute } from '../../models';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +14,27 @@ export class SeoService {
   constructor(
     private title: Title,
     private meta: Meta,
-    private platformService: PlatformService
+    private platformService: PlatformService,
+    private router: Router
   ) {}
+
+  seoForMainRoutes(routes: IRoute[]) {
+    this.router.events
+      .pipe(
+        filter((event: RouterEvent) => event instanceof NavigationEnd),
+        map(navigationEndEvent => navigationEndEvent.url),
+        map(url => routes.find(route => route.path === url))
+      )
+      .subscribe(route => {
+        if (route) {
+          this.updateTitle(route.title);
+          this.updateUrl(route.path);
+          this.updateType(route.type);
+          this.updateDescription(route.description);
+          this.updateImageUrl(route.imageUrl);
+        }
+      });
+  }
 
   updateTitle(title: string) {
     title = `${this.appName} | ${title}`;
