00085b6571f90fe7986180416222ce43d9707eff
batbrain9392 <debmallyabhattacharya@gmail.com>
2019-09-12 15:40:29 +0530

    feat(ssr): added fastify

--- server/main.ts
+++ server/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { ApplicationModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ApplicationModule);
+  const app = await NestFactory.create<NestFastifyApplication>(ApplicationModule,
+    new FastifyAdapter());
   app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT || 4200);
+  await app.listen(process.env.PORT || 4200, '0.0.0.0');
 }
 bootstrap();
