eaae36d594493d3997d34d5637e03224d25288d8
batbrain9392 <debmallyabhattacharya@gmail.com>
2019-09-12 15:42:57 +0530

    fix(share): snackbar not required

--- src/app/shared/services/web-share/web-share.service.ts
+++ src/app/shared/services/web-share/web-share.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { MatSnackBar } from '@angular/material';
 import { PlatformService } from '../platform/platform.service';
 import { IWebShare } from './i-web-share';
 
@@ -9,19 +8,13 @@ import { IWebShare } from './i-web-share';
 export class WebShareService {
   readonly isWebShareAvailable: boolean;
 
-  constructor(platformService: PlatformService, private matSnackBar: MatSnackBar) {
+  constructor(platformService: PlatformService) {
     if (platformService.isBrowser) {
       this.isWebShareAvailable = !!(navigator && (navigator as any).share);
     }
   }
 
   share({ title, text, url }: IWebShare) {
-    (navigator as any)
-      .share({ title, text, url })
-      .then(() => this.matSnackBar.open('Shared.'))
-      .catch((error: any) => {
-        this.matSnackBar.open('Something went wrong! Please try again later.');
-        console.log('Error sharing', error);
-      });
+    (navigator as any).share({ title, text, url });
   }
 }
